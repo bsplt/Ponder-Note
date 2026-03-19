9ac22f0c177973dd57ddc10f57ef3e249eb3be7a
Alexander Lehmann <a.lehmann@protonmail.com>
2026-03-19 21:00:34 +0100

    fix store issue

--- flake.nix
+++ flake.nix
@@ -23,6 +23,12 @@
         # macOS SDK aus Xcode CLT verfügbar machen
         export SDKROOT="$(xcrun --sdk macosx --show-sdk-path)"
         export DEVELOPER_DIR="$(xcode-select -p)"
+        export CC="$(xcrun --sdk macosx -f clang)"
+        export CXX="$(xcrun --sdk macosx -f clang++)"
+        export AR="$(xcrun --sdk macosx -f ar)"
+        export RANLIB="$(xcrun --sdk macosx -f ranlib)"
+        export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER="$CC"
+        export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER="$CC"
 
         # Komfort
         export RUST_BACKTRACE=1
